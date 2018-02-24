3d3cb0466a44771adc40dc75e2e6757ec1afbdff
徐淳 <bluexuchun@gmail.com>
2018-02-24 22:26:29 +0800

    修改
    修改了全屏滚动

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function change(){
+  $('.DrotZ').removeClass('rotz').removeClass('rotz-ori').addClass('rotz');
+  $('.DrotZ-back').removeClass('rotz-back').removeClass('rotz-back-ori').addClass('rotz-back');
+}
+function changeOrigin(){
+  $('.DrotZ').removeClass('rotz').removeClass('rotz-ori').addClass('rotz-ori');
+  $('.DrotZ-back').removeClass('rotz-back').removeClass('rotz-back-ori').addClass('rotz-back-ori');
+}
